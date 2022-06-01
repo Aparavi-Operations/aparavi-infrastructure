0907e33e142ced54912affc1fa8ad5fe48d3af65
plapin-aparavi <pavel.lapin@aparavi.com>
2022-06-01 18:56:00 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -84,18 +84,18 @@ fi
 
 ########################
 
-# sed -i 's/deb cdrom/#deb cdrom/' /etc/apt/sources.list
-# apt update
-# apt install ansible git sshpass vim python3-mysqldb -y
+sed -i 's/deb cdrom/#deb cdrom/' /etc/apt/sources.list
+apt update
+apt install ansible git sshpass vim python3-mysqldb -y
 
-# ### Make sure target directory exists and empty
-# mkdir $INSTALL_TMP_DIR
-# cd $INSTALL_TMP_DIR
-# [ -d "./aparavi-infrastructure" ] && rm -rf ./aparavi-infrastructure
-# git clone https://github.com/Aparavi-Operations/aparavi-infrastructure.git
-# cd aparavi-infrastructure/ansible/
-# export ANSIBLE_ROLES_PATH="$INSTALL_TMP_DIR/aparavi-infrastructure/ansible/roles/"
-# ansible-galaxy install -r roles/requirements.yml
+### Make sure target directory exists and empty
+mkdir $INSTALL_TMP_DIR
+cd $INSTALL_TMP_DIR
+[ -d "./aparavi-infrastructure" ] && rm -rf ./aparavi-infrastructure
+git clone https://github.com/Aparavi-Operations/aparavi-infrastructure.git
+cd aparavi-infrastructure/ansible/
+export ANSIBLE_ROLES_PATH="$INSTALL_TMP_DIR/aparavi-infrastructure/ansible/roles/"
+ansible-galaxy install -r roles/requirements.yml
 
 
 ansible-playbook --connection=local $INSTALL_TMP_DIR/aparavi-infrastructure/ansible/playbooks/base/main.yml -i 127.0.0.1, $VERBOSE \

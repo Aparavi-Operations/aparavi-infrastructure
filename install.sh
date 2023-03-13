ad3535aacbee88066279cba0ea0f1ca30a40c873
Ilya Taskaev <Ilya.Taskaev@aparavi.com>
2023-03-13 15:23:13 +0100

    added debug

--- install.sh
+++ install.sh
@@ -193,7 +193,7 @@ export ANSIBLE_ROLES_PATH="$INSTALL_TMP_DIR/aparavi-infrastructure/ansible/roles
 ansible-galaxy install -r roles/requirements.yml
 
 ###### run ansible ######
-ansible-playbook --connection=local $INSTALL_TMP_DIR/aparavi-infrastructure/ansible/playbooks/base/main.yml -i 127.0.0.1, $VERBOSE $NODE_ANSIBLE_TAGS \
+ansible-playbook --connection=local $INSTALL_TMP_DIR/aparavi-infrastructure/ansible/playbooks/base/main.yml -vvv -i 127.0.0.1, $VERBOSE $NODE_ANSIBLE_TAGS \
     --extra-vars    "mysql_appuser_name=$MYSQL_APPUSER_NAME \
                     aparavi_platform_bind_addr=$APARAVI_PLATFORM_BIND_ADDR \
                     aparavi_platform_addr=$APARAVI_PLATFORM_ADDR \

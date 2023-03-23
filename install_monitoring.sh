48fa758f9fd9c5f327993c1fd75e2e05dce7017a
Konstantin Kornienko <konstantin.kornienko@aparavi.com>
2023-03-23 16:52:19 +0000

    fix

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -5,7 +5,8 @@ INSTALL_TMP_DIR=~
 cd "$INSTALL_TMP_DIR/aparavi-infrastructure/ansible"
 export ANSIBLE_ROLES_PATH="$INSTALL_TMP_DIR/aparavi-infrastructure/ansible/roles"
 
-ansible-playbook \
+pipenv install --skip-lock
+pipenv run ansible-playbook \
   --connection=local \
   -i 127.0.0.1, \
   playbooks/monitoring/main.yml

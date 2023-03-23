b8eab0a5cc91f09755c647cc5c1da4da8c796ce8
Konstantin Kornienko <konstantin.kornienko@aparavi.com>
2023-03-23 17:21:18 +0000

    fix

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+function get_local_ip() {
+  for ip in $(/usr/bin/hostname -I); do
+    if [[ $ip != 10.* && $ip != 172.17.* ]]; then
+      echo $ip
+      return
+    fi
+  done
+}
+
 INSTALL_TMP_DIR=~
 
 cd "$INSTALL_TMP_DIR/aparavi-infrastructure/ansible"
 
 export ENVIRONMENT=ohio
 export SERVICE_INSTANCE=automation_test
+export PLATFORM_IP="$(get_local_ip)"
+
 
 export ANSIBLE_ROLES_PATH="$INSTALL_TMP_DIR/aparavi-infrastructure/ansible/roles"
 export PIPENV_PIPFILE="$INSTALL_TMP_DIR/aparavi-infrastructure/Pipfile"
@@ -21,4 +32,4 @@ pipenv install --skip-lock
 #   playbooks/monitoring/main.yml
 
 cd "$INSTALL_TMP_DIR/aparavi-infrastructure/monitoring"
-pipenv run jinja -E ENVIRONMENT vmagent/scrape.yml.j2
+pipenv run jinja -E ENVIRONMENT -E PLATFORM_IP vmagent/scrape.yml.j2 -o vmagent/scrape.yml

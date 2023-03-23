be1b48bcf3687eef28b62c55408fb98ceea9b8c9
Konstantin Kornienko <konstantin.kornienko@aparavi.com>
2023-03-23 17:10:19 +0000

    fix

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -3,13 +3,22 @@
 INSTALL_TMP_DIR=~
 
 cd "$INSTALL_TMP_DIR/aparavi-infrastructure/ansible"
+
+export ENVIRONMENT=ohio
+export SERVICE_INSTANCE=automation_test
+
 export ANSIBLE_ROLES_PATH="$INSTALL_TMP_DIR/aparavi-infrastructure/ansible/roles"
+export PIPENV_PIPFILE="$INSTALL_TMP_DIR/aparavi-infrastructure/ansible/Pipfile"
 
 # Install pipenv
 apt install -y pipenv
 pipenv install --skip-lock
 
-pipenv run ansible-playbook \
-  --connection=local \
-  -i 127.0.0.1, \
-  playbooks/monitoring/main.yml
+# Install docker
+# pipenv run ansible-playbook \
+#   --connection=local \
+#   -i 127.0.0.1, \
+#   playbooks/monitoring/main.yml
+
+cd "$INSTALL_TMP_DIR/aparavi-infrastructure/monitoring"
+pipenv run jinja -E ENVIRONMENT vmagent/scrape.yml.j2

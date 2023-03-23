945556fcc847e7d2f22e5766082efbde82bcddb9
Konstantin Kornienko <konstantin.kornienko@aparavi.com>
2023-03-23 16:53:42 +0000

    fix

--- install_monitoring.sh
+++ install_monitoring.sh
@@ -5,7 +5,10 @@ INSTALL_TMP_DIR=~
 cd "$INSTALL_TMP_DIR/aparavi-infrastructure/ansible"
 export ANSIBLE_ROLES_PATH="$INSTALL_TMP_DIR/aparavi-infrastructure/ansible/roles"
 
+# Install pipenv
+apt install -y pipenv
 pipenv install --skip-lock
+
 pipenv run ansible-playbook \
   --connection=local \
   -i 127.0.0.1, \
